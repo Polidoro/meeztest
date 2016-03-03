05732fd6392d882155d54f0c4d498bada2aff058
Vincent Polidoro <polidoro@gmail.com>
2016-03-02 19:14:11 -0500

    Initial Commit

---
+++ MeezTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MeezTest
+//
+//  Created by Vincent Polidoro on 3/2/16.
+//  Copyright © 2016 Vincent Polidoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
